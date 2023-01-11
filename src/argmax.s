b630d48bf8f1df65a4dbd72f760e427ba0f8488e
rda <2055017970@qq.com>
2023-01-11 17:25:23 +0800

    part_a task3

--- src/argmax.s
+++ src/argmax.s
@@ -15,16 +15,31 @@
 #     this function terminates the program with error code 36
 # =================================================================
 argmax:
+    ebreak
     # Prologue
+    add t0, a0, x0   #t0 is the address of current item
+    add t1, x0, x0   # t1 is the current index
 
+    # if a1 greater than 1, goto loop_start
+    bgt a1, x0, loop_start
+    addi a0, x0, 36
+    j exit
 
 loop_start:
-
-
+    lw t2, 0(t0)
+    beq t1, x0, update
+    bge t3, t2, loop_continue  # if now_biggest >= cur value goto continue
+update:
+    add t3, t2, x0
+    add a0, t1, x0
 loop_continue:
-
+    addi t1, t1, 1
+    addi t0, t0, 4
+    beq t1, a1, loop_end
+    j loop_start
 
 loop_end:
     # Epilogue
+
 
     jr ra
