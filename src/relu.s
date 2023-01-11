fd43e8ab618bc31608a097c16b231a6d5989098e
rda <2055017970@qq.com>
2023-01-11 17:15:49 +0800

    part_a task2

--- src/relu.s
+++ src/relu.s
@@ -13,25 +13,28 @@
 #     this function terminates the program with error code 36
 # ==============================================================================
 relu:
+    ebreak
     # Prologue
+    add t0, a0, x0   #t0 is the address of current item
+    add t1, x0, x0   # t1 is the current index
 
+    # if a1 greater than 1, goto loop_start
+    bgt a1, x0, loop_start
+    addi a0, x0, 36
+    j exit
 
 loop_start:
-
-
-
-
-
-
-
-
+    lw t2, 0(t0)
+    bgt t2, x0, loop_continue
+    sub t2, t2, t2
 loop_continue:
-
-
+    sw t2, 0(t0)
+    addi t1, t1, 1
+    addi t0, t0, 4
+    beq t1, a1, loop_end
+    j loop_start
 
 loop_end:
-
-
     # Epilogue
 
 
