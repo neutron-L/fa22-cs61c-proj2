d64b25932cc773ab9fd4dc3f5f59da03bc83e08f
rda <2055017970@qq.com>
2023-01-11 23:10:47 +0800

    part_a

--- src/dot.s
+++ src/dot.s
@@ -17,28 +17,66 @@
 #   - If the stride of either array is less than 1,
 #     this function terminates the program with error code 37
 # =======================================================
+
+
 dot:
-
     # Prologue
+    add t0, x0, x0 # t0 store the sum
+    add t1, x0, x0 # current index of arr1
+    add t2, x0, x0 # current index of arr2
+    add t6, x0, x0 # current used items number
 
+    # check length
+    bgt a2, x0, check_stride1
+    addi a0, x0, 36
+    j error 
+check_stride1:
+    # check arr1 stride  
+    bgt a3, x0, check_stride2
+    j set_code
+check_stride2:
+    # check arr2 stride   
+    bgt a4, x0, loop_start
+    
+set_code:
+    addi a0, x0, 37
+error:
+    j exit
 
 loop_start:
+    # load arr1[t1]
+    slli t3, t1, 2
+    add t3, t3, a0
+    lw t4, 0(t3)
 
+    # load arr2[t2]
+    slli t3, t2, 2
+    add t3, t3, a1
+    lw t5, 0(t3)
 
+    # sum += t4 * t5
+    mul t4, t4, t5
+    add t0, t0, t4
 
-
-
-
-
-
-
-
-
+loop_continue:
+    add t1, t1, a3
+    add t2, t2, a4
+    addi t6, t6, 1
+    bge t6, a2, loop_end
+    j loop_start
 
 loop_end:
 
 
     # Epilogue
 
-
+    add a0, t0, x0
     jr ra
+
+
+# =======================================================
+# FUNCTION: check if arg is less than 1
+# Arguments:
+#   a0 (int) is a number
+#   a1 (int) is the error exit code if a0 <= 0
+# =======================================================
--- src/matmul.s
+++ src/matmul.s
@@ -24,40 +24,87 @@
 #     this function terminates the program with exit code 38
 # =======================================================
 matmul:
-
+ebreak
     # Error checks
-
+    bne a2, a4, error
+    bge x0, a1, error
+    bge x0, a2, error
+    bge x0, a4, error
+    bge x0, a5, error
 
     # Prologue
-
+    add t1, x0, x0  # t1 is i
+    add t2, x0, x0  # t2 is j
 
 outer_loop_start:
-
-
+    # if i >= a1 goto outer_loop_end
+    bge t1, a1, outer_loop_end
+    add t0, a3, x0  # t0 point to the m1, call it pb
+    sub t2, t2, t2  # j = 0
 
 
 inner_loop_start:
+    # if j >= a5 goto inner_loop_end
+    bge t2, a5, inner_loop_end
 
+    # save param and temporal register
+    addi sp, sp, -44
+    sw a0, 0(sp)
+    sw a1, 4(sp)
+    sw a2, 8(sp)
+    sw a3, 12(sp)
+    sw a4, 16(sp)
+    sw a5, 20(sp)
+    sw a6, 24(sp)
+    sw t0, 28(sp)
+    sw t1, 32(sp)
+    sw t2, 36(sp)
+    sw ra, 40(sp)
 
+    # set param
+    add a0, a0, x0
+    add a1, t0, x0
+    add a2, a2, x0
+    addi a3, x0, 1 
+    add a4, a5, x0
 
+    # call dot
+    jal x1, dot
+    lw a6, 24(sp)
+    sw a0, 0(a6)          # *pc = a6
 
+    # restore param and temporal register
+    lw a0, 0(sp)
+    lw a1, 4(sp)
+    lw a2, 8(sp)
+    lw a3, 12(sp)
+    lw a4, 16(sp)
+    lw a5, 20(sp)
+    lw t0, 28(sp)
+    lw t1, 32(sp)
+    lw t2, 36(sp)
+    lw ra, 40(sp)
 
+    addi sp, sp, 44
 
+    addi t2, t2, 1        # ++j, ++pc, ++pb 
+    addi a6, a6, 4
+    addi t0, t0, 4
 
-
-
-
-
+    j inner_loop_start
 
 inner_loop_end:
-
-
-
+    slli a2, a2, 2
+    add a0, a0, a2
+    srai a2, a2, 2
+    addi t1, t1, 1
+    j outer_loop_start
 
 outer_loop_end:
-
-
     # Epilogue
 
-
     jr ra
+
+error:
+    addi a0, x0, 38
+    j exit
