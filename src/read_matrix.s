6c067b4113cad619a53a2fd9c30b02b5eb006182
rda <2055017970@qq.com>
2023-01-12 17:09:52 +0800

    part_b task 7

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -25,18 +25,92 @@
 #     this function terminates the program with error code 29
 # ==============================================================================
 read_matrix:
-
+    ebreak
     # Prologue
+    addi sp, sp, -20
+    sw ra, 0(sp)
+    sw a1, 4(sp)
+    sw a2, 8(sp)
+    sw s0, 12(sp)
+    sw s1, 16(sp)
 
+    # open file
+    add a1, x0, x0
+    jal x1, fopen
+    addi a1, x0, -1
+    beq a0, a1, fopen_error
+    add s0, a0, x0
+    
+    # read rows and cols
+    lw a1, 4(sp)
+    addi a2, x0, 4
+    jal x1, fread 
+    addi a2, x0, 4
+    bne a0, a2, fread_error
 
+    add a0, s0, x0
+    lw a1, 8(sp)
+    addi a2, x0, 4
+    jal x1, fread 
+    addi a2, x0, 4
+    bne a0, a2, fread_error
 
+    # malloc space
+    lw a1, 4(sp)
+    lw t3, 0(a1)
+    lw a1, 8(sp)
+    lw t4, 0(a1)
+    mul s1, t3, t4
+    slli s1, s1, 2
+    add a0, s1, x0
+    jal x1, malloc
+    beq a0, x0, malloc_error
 
+    add a1, a0, x0    # pointer
+    add a0, s0, x0    # a0 is the fp
+    add a2, s1, x0    
 
+    # save fp
+    addi sp, sp, -4
+    sw s0, 0(sp)
 
+    add s0, a1, x0    # s0 is the matrix
+    # read matrix
+    jal x1, fread
+    bne a0, s1, fread_error
 
+    # restore fp
+    lw a0, 0(sp)
+    addi sp, sp, 4
 
+    # close file
+    jal x1, fclose
+    bne a0, x0, fclose_error 
+    add a0, s0, x0
 
     # Epilogue
-
+    lw ra, 0(sp)
+    lw a1, 4(sp)
+    lw a2, 8(sp)
+    lw s0, 12(sp)
+    lw s1, 16(sp)
+    addi sp, sp, 20
 
     jr ra
+
+
+malloc_error:
+    addi a0, x0, 26
+    j exit
+
+fopen_error:
+    addi a0, x0, 27
+    j exit
+
+fclose_error:
+    addi a0, x0, 28
+    j exit
+
+fread_error:
+    addi a0, x0, 29
+    j exit
